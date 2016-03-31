4f318275fd958b68bbb4f8e0a08dd3f52fb5ad56
novneetnov <novneet346iith@gmail.com>
2016-03-31 11:18:33 +0530

    Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+	
+  get 'dashboard/index'
+
+	root "dashboard#index"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
