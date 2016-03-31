6668625acb8f95656f753d9728f0a3c9345c5b98
novneetnov <novneet346iith@gmail.com>
2016-03-31 18:50:03 +0530

    Style Devise error messages and flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	
+	def devise_error_messages!
+		return "" if resource.errors.empty?
+
+		messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+		html = <<-HTML
+		<div class="alert alert-dismissible alert-error alert-danger" role="alert">
+			<a href="#", class="close", data-dismiss="alert">&times;</a>
+			<%= content_tag :div, msg if msg.is_a?(String) %>
+		</div>
+		HTML
+		html.html_safe
+	end
+
 end
