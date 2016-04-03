ea4f65a3433175d12506f1323337bcc5ea853456
novneetnov <novneet346iith@gmail.com>
2016-04-03 16:58:03 +0530

    Implement Destroy Action

---
+++ spec/features/deleting_exercises_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.feature "Deleting Exercise" do
+
+	before do
+		@owner = User.create(email: "owner@example.com", password: "password")
+		login_as(@owner)
+		@owner_exer = @owner.exercises.create!(duration_in_min: 48, workout: "Cardio special", workout_date: Date.today)
+	end
+
+	scenario do
+		visit "/"
+		click_link "My Lounge"
+		link = "//a[contains(@href, '/users/#{@owner.id}/exercises/#{@owner_exer.id}') and @data-method='delete']"
+		find(:xpath, link).click
+		#click_link "Delete"
+		expect(page).to have_content("Exercise has been deleted")
+	end
+	
+end
