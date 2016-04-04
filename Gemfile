bb854fe22cf25c9b1093281abbdd5f5284d3adb1
novneetnov <novneet346iith@gmail.com>
2016-04-04 15:27:25 +0530

    Add pagination to Users

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'autoprefixer-rails'
 
 gem 'devise', '~> 3.5', '>= 3.5.6'
 gem 'jquery-ui-rails', '~> 5.0', '>= 5.0.5'
+gem 'will_paginate-bootstrap', '~> 1.0', '>= 1.0.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :exercises
-
 	validates :last_name, presence: true
+	self.per_page = 4
 
 	def full_name
 		"#{first_name if !first_name.nil?} #{last_name}".strip
